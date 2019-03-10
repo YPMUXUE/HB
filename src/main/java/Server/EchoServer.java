87ba3d2879af2f871ae0a267859470f13c63484a
YPMUXUE <yuanpan95@foxmail.com>
2019-03-11 01:49:32 +0800

    commit

--- src/main/java/Server/EchoServer.java
+++ src/main/java/Server/EchoServer.java
@@ -1,37 +1,40 @@
 package Server;
 
-import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.*;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws Exception {
         ServerBootstrap serverBootstrap=new ServerBootstrap();
         serverBootstrap.group(ProxyServer.eventLoopGroup)
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInboundHandlerAdapter(){
-                    @Override
-                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                        System.out.println(ByteBufUtil.hexDump((ByteBuf) msg));
-                        super.channelRead(ctx,msg);
-                    }
+                .childHandler(new ChannelInitializer<Channel>() {
 
                     @Override
-                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                        super.channelActive(ctx);
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println(((ByteBuf)msg).toString(StandardCharsets.UTF_8));
+                                super.channelRead(ctx,msg);
+                            }
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                super.channelActive(ctx);
+                            }
+                        });
                     }
                 });
         InetAddress localAddr=InetAddress.getLocalHost();
-        SocketAddress address=new InetSocketAddress(InetAddress.getLoopbackAddress(),4449);
+        SocketAddress address=new InetSocketAddress("192.168.36.178",4449);
         ChannelFuture future = serverBootstrap.bind(address);
         future.syncUninterruptibly();
     }
