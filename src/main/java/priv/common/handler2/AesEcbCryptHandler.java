a1ca8b373268b3007261bd923a2c265f0177b6c5
yuanpan <yuanpan95@foxmail.com>
2019-10-15 17:26:32 +0800

    add

--- src/main/java/priv/common/handler2/AesEcbCryptHandler.java
+++ src/main/java/priv/common/handler2/AesEcbCryptHandler.java
@@ -1,6 +1,7 @@
 package priv.common.handler2;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandler;
@@ -8,23 +9,56 @@ import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.CorruptedFrameException;
 import io.netty.util.ReferenceCountUtil;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import priv.common.crypto.AesCrypto;
 
 import java.net.SocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- *  * @author  pyuan
- *  * @date    2019/10/12 0012
- *  * @Description
- *  *
+ * +---------------------encrypt--------------------------------------------+----------------------encrypt-------------------------+
+ * |                                                                        |                                                      |
+ * +------------------------------------------------------------------------+------------------------------------------------------+
+ * |                 header(48)                                             |                    content                           |
+ * |  0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                              |                   "0xXXXXXX"                         |
+ * +------------------------------------------------------------------------+------------------------------------------------------+
+ * <p>
+ *              +---------------------Digest----------------------+
+ *              |                                                 |
+ * +------------+----------------------+--------------------------+-----------------------------------------------------+
+ * | Digest(32) |  content Length(4)   |    protocol sign(12)     |                       content                       |
+ * |  0xXXXX    |    0x0000000X        |    0xXXXXXXXX            |                        0xXXXX                       |
+ * +------------+----------------------+--------------------------+-----------------------------------------------------+
+ * <p>
+ *                                                                                      +------------Digest-------------+
+ *                                                                                      |                               |
+ * +------------+----------------------+--------------------------+---------------------+-------------------------------+
+ * | Digest(32) |  content Length(4)   |    protocol sign(12)     |    Digest(32)       |          content              |
+ * |  0xXXXX    |    0x0000000X        |    0xXXXXXXXX            |    0xXXXXXXXX       |           "1234"              |
+ * +------------+----------------------+--------------------------+---------------------+-------------------------------+
+ * <p>
+ *  
+ *  
+ *  
  *  
  */
 public class AesEcbCryptHandler extends ByteToMessageDecoder implements ChannelOutboundHandler {
-	private static final int FRAME_HEADER_LENGTH = 64;
-	private static final int LENGTH_OFFSET = 32;
+	private static final int FRAME_HEADER_LENGTH = 48;
+	private static final int DIGEST_LENGTH = 32;
+	private static final int CONTENT_LENGTH_OFFSET = DIGEST_LENGTH;
+	private static final byte[] PROTOCOL_SIGN;
+	private static final int PROTOCOL_SIGN_OFFSET = CONTENT_LENGTH_OFFSET + 4;
 	private final AesCrypto aesCrypto;
 	private byte[] headerBytes;
+
+	static {
+		PROTOCOL_SIGN = "123456789012".getBytes(StandardCharsets.US_ASCII);
+		assert PROTOCOL_SIGN.length == 12;
+	}
 
 	public AesEcbCryptHandler(AesCrypto aesCrypto) {
 		this.aesCrypto = aesCrypto;
@@ -43,21 +77,25 @@ public class AesEcbCryptHandler extends ByteToMessageDecoder implements ChannelO
 	 * @see io.netty.handler.codec.LengthFieldBasedFrameDecoder#decode(ChannelHandlerContext, ByteBuf)
 	 */
 	private ByteBuf splitFrame(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
-		if (headerBytes == null){
-			if (in.readableBytes() < FRAME_HEADER_LENGTH){
+		if (headerBytes == null) {
+			if (in.readableBytes() < FRAME_HEADER_LENGTH) {
 				return null;
 			}
 			byte[] encryptedHeaderBytes = new byte[FRAME_HEADER_LENGTH];
 			in.readBytes(FRAME_HEADER_LENGTH);
 			this.headerBytes = this.aesCrypto.decrypt(encryptedHeaderBytes);
 		}
-		int frameLength = bytesToInt(headerBytes, LENGTH_OFFSET);
-		if (frameLength <= 0){
-			throw new CorruptedFrameException("frame Length field is less then 0:"+frameLength);
+		int frameLength = bytesToInt(headerBytes, CONTENT_LENGTH_OFFSET);
+		if (frameLength <= 0) {
+			throw new CorruptedFrameException("frame Length field is less then 0:" + frameLength);
+		}
+		if (!arraysEquals(this.headerBytes,PROTOCOL_SIGN_OFFSET,PROTOCOL_SIGN.length,PROTOCOL_SIGN)){
+			throw new CorruptedFrameException("wrong sign of frame. headers:" + Hex.encodeHexString(headerBytes));
 		}
 		if (in.readableBytes() < frameLength) {
 			return null;
 		}
+
 
 		return null;
 
@@ -138,5 +176,19 @@ public class AesEcbCryptHandler extends ByteToMessageDecoder implements ChannelO
 				| ((src[offset + 2] & 0xFF) << 8)
 				| (src[offset + 3] & 0xFF));
 		return value;
+	}
+	private static boolean arraysEquals(byte[] array1,int array1Offset, int array1Length, byte[] array2) {
+		if (array1 == null || array2 == null){
+			return false;
+		}
+		if (array1Length != array2.length){
+			return false;
+		}
+		for (int i = 0; i < array1Length; i++) {
+			if (array1[i+array1Offset] != array2[i]){
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -13,29 +13,9 @@ import java.nio.charset.Charset;
 
 public class ClientTest {
     public static void main(String[] args) throws Exception {
-        Bootstrap bootstrap=new Bootstrap();
-        EventLoopGroup eventLoopGroup=new NioEventLoopGroup();
-        bootstrap.group(eventLoopGroup)
-                .channel(NioSocketChannel.class)
-                .handler(new SimpleChannelInboundHandler<ByteBuf>() {
-                    private int i=0;
-                    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-                        byte[] buffer=new byte[msg.readableBytes()];
-                        msg.readBytes(buffer);
-                        System.out.println(new String(buffer,0,buffer.length,"UTF-8"));
-//                        System.out.println(ByteBufUtil.hexDump(buffer));
-                    }
-                });
-        ChannelFuture future1 = bootstrap.connect(InetAddress.getByName("www.baidu.com"), 80);
-        future1.addListener((future) -> {
-            if (future.isSuccess()) {
-                System.out.println("connect success");
-                future1.channel().writeAndFlush(Unpooled.copiedBuffer("GET / HTTP/1.1\r\nHost:www.baidu.com\r\n\r\n",Charset.forName("UTF-8")));
-
-            }
-        });
-         future1.channel().closeFuture().addListener((future -> eventLoopGroup.shutdownGracefully()));
-
-
+        Long x = 12345678L;
+        x = x+1;
+        x=null;
+        System.out.println(x+"");
     }
 }
